d3b131829bf712bef730b888328cffc58dc4ea92
Infinitymaths <sharath.infinitymaths@gmail.com>
2021-11-04 23:20:23 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 18:03:04 2021
+
+@author: SHARATH
+"""
+
+from nltk.tokenize import word_tokenize
+
+sentence = 'hey jarvis what is the date today'
+
+output = word_tokenize(sentence)
+
+print(output)
